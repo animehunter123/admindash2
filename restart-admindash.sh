fa206fd2b7d3f3cef8a8fc84d71750397e386c03
animehunter123 <42163211+animehunter123@users.noreply.github.com>
2025-10-01 11:58:29 +0900

    Added a note to the user so he knows its running on 5001

--- restart-admindash.sh
+++ restart-admindash.sh
@@ -7,3 +7,9 @@ docker-compose up -d
 # New Docker
 docker compose down
 docker compose up -d
+
+docker ps -a | grep -i admindash2-app
+
+echo "Attempted to launch it via docker-compose or docker compose...
+
+So now at this point it should be running as localhost:5001"
