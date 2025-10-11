88cd6ac0699c4205b4384ff969a39ae4125c2fcc
animehunter123 <42163211+animehunter123@users.noreply.github.com>
2025-10-12 00:30:49 +0900

    added docker-compose for ancient wsl

--- Dockerfile_InstallDockerCeOnHost.sh
+++ Dockerfile_InstallDockerCeOnHost.sh
@@ -3,10 +3,14 @@
 echo "Please make sure to actually read the official documentation before running this script
 https://docs.docker.com/engine/install/ubuntu/
 "
+
 if ! command -v docker &>/dev/null ; then 
   echo Docker not installed ; 
   apt update
   apt install -y docker.io docker-compose 
 fi
+
+echo "docker command now is installed, installing legacy docker-compose as well for wsl mitigation."
+pip3 install docker-compose --break-system-packages
 
 echo "Install complete."
