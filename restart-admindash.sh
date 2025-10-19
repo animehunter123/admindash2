c16cfdcc53ab6bf4f7fb3e7d46598a1779e0b3a2
animehunter123 <42163211+animehunter123@users.noreply.github.com>
2025-10-19 12:32:26 +0900

    Cleaned up logging, added Ub2004 until Rocky10 possibilities.

--- restart-admindash.sh
+++ restart-admindash.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-echo "Attempting to restart the admin dash... via docker-compose or docker compose... please ignore errors and warnings. See the end of the script for details, and check webpage."
+echo "Admindash2: Attempting to restart the admindash webpage...
+This is via docker-compose or docker compose...
+
+STARTING..."
 
 # Old Docker
-sudo docker-compose down
-sudo docker-compose up -d
+sudo docker-compose down 2>/dev/null
+sudo docker-compose up -d 2>/dev/null
+/usr/local/bin/docker-compose down 2>/dev/null
+/usr/local/bin/docker-compose up -d 2>/dev/null
 
 # New Docker
-sudo docker compose down
-sudo docker compose up -d
+sudo docker compose down 2>/dev/null
+sudo docker compose up -d 2>/dev/null
 
+# Searching to see if it started to inform the user
 sudo docker ps -a | grep -i admindash2-app
 
-clear; 
+echo "OK...
 
-echo "Attempted to launch it via docker-compose or docker compose...
+Attempted to launch it via docker-compose or docker compose (all possible commands from 20.04>rocky9)...
 
-So now at this point it should be running as http://$(hostname -I | awk '{print $1}'):5001/"
+So now at this point it should be running as http://$(hostname -I | awk '{print $1}'):5001/
+
+Script complete."
